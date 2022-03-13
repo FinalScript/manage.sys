1257fc7198a701594a545390f7ee86a96eab1672
moe1011 <77053716+moe1011@users.noreply.github.com>
2022-03-12 21:39:34 -0500

    For EmployeeService and EmployeeRepositroy - Updated functionality for getEmployees method - Added a new repository to fine employees by store ID

--- server/src/main/java/com/finalscript/storemanagementapi/services/EmployeeService.java
+++ server/src/main/java/com/finalscript/storemanagementapi/services/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.finalscript.storemanagementapi.services;
 
+import com.finalscript.storemanagementapi.models.Employee;
 import com.finalscript.storemanagementapi.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -13,15 +18,24 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
-    public void getEmployees() {
-        // TODO
+    public List<Employee> getEmployees(Long storeId) {
+        List<Employee> employeeList = employeeRepository.findEmployeesByStore_Id(storeId);
+
+        if (employeeList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No employees found at store #" + storeId);
+        }
+
+        return employeeList;
     }
 
-    public void getEmployee() {
+    public Employee getEmployee() {
         // TODO
+        
+        return null;
     }
 
-    public void newEmployee() {
+    public Employee newEmployee() {
         // TODO
+        return null;
     }
 }
