8ad18939c1265342cc427a149a3ad9a926a96c3c
Bakr Matlab <matlab893@gmail.com>
2022-03-17 19:23:54 -0400

    Added javadoc comments to the following controllers -AdminUserController -StoreController -EmployeeController

--- server/src/main/java/com/finalscript/storemanagementapi/controllers/AdminUserController.java
+++ server/src/main/java/com/finalscript/storemanagementapi/controllers/AdminUserController.java
@@ -7,16 +7,25 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Handle the AdminUser Controllers
+ */
 @RestController
 @RequestMapping(path = "/api/v1/admin")
 public class AdminUserController {
     private final AdminUserService adminUserService;
 
+    /**
+     * @param adminUserService adminUserService  Class object
+     */
     @Autowired
     public AdminUserController(AdminUserService adminUserService) {
         this.adminUserService = adminUserService;
     }
 
+    /**
+     * @return  Retrieved admin at the given ID
+     */
     @GetMapping
     public AdminUser getAdmin() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -24,6 +33,11 @@ public class AdminUserController {
         return adminUserService.getAdmin(Long.valueOf(authentication.getPrincipal().toString()));
     }
 
+    /**
+     * @param username username of admin
+     * @param password password of admin
+     * @return A new registered admin user
+     */
     @PostMapping("/register")
     public AdminUser register(@RequestParam String username, @RequestParam String password) {
         AdminUser newUser = new AdminUser(username, password);
@@ -31,6 +45,11 @@ public class AdminUserController {
         return adminUserService.register(newUser);
     }
 
+    /**
+     * @param username username of admin
+     * @param password password of admin
+     * @return The Logged in admin user
+     */
     @PostMapping("/login")
     public AdminUser login(@RequestParam String username, @RequestParam String password) {
         AdminUser newUser = new AdminUser(username, password);
