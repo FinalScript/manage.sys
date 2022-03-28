1dde1c0cf0f341d3d48b466bf274f24be4679c98
Roynul Rohan <roynulrohan@gmail.com>
2022-03-28 04:30:09 -0400

    Created prettier config file

--- client/src/constants/actions.ts
+++ client/src/constants/actions.ts
@@ -7,3 +7,4 @@ export const ADD_STORES = 'ADD_STORES';
 export const REMOVE_STORES = 'REMOVE_STORES';
 export const ADD_STORE = 'ADD_STORE';
 export const REMOVE_STORE = 'REMOVE_STORE';
+export const UPDATE_STORE = 'UPDATE_STORE';
