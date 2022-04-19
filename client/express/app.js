0fc50986ccee602f50572d47674ccddd55e261a0
Roynul Rohan <roynulrohan@gmail.com>
2022-04-19 07:32:13 -0400

    Deployment update

---
+++ client/express/app.js
@@ -0,0 +1,20 @@
+const express = require('express'); //Line 1
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv')
+const app = express(); //Line 2
+const port = process.env.PORT || 3000; //Line 3
+
+dotenv.config();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 interface registerParams {
     username: String;
