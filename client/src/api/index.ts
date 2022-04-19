0f30b946dc3df5bca2f49dd83b0eb8f457d64f24
Roynul Rohan <roynulrohan@gmail.com>
2022-04-19 06:12:11 -0400

    Docker changes

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://managa-sys-api.azurewebsites.net/' });
 
 interface registerParams {
     username: String;
